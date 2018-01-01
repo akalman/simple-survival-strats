b05b1e6b3124f138f6c53fc06a8a24b52bb1d3a2
Adam Kalman <dkeka.zkfaks@gmail.com>
2018-01-01 14:06:21 -0800

    generated mod template

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple Survival Strats
---
+++ SimpleSurvivalStrats.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+namespace SimpleSurvivalStrats
+{
+	class SimpleSurvivalStrats : Mod
+	{
+		public SimpleSurvivalStrats()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+	}
+}
